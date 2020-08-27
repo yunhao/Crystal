5935783c33cbaf929434dee96e150d44307488a4
yunhao <rhetttuan@gmail.com>
2020-08-27 16:12:08 +0800

    Update code comment

--- Sources/Crystal.swift
+++ Sources/Crystal.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+
+/// The main Crystal class. Instead of creating a `Crystal` object, you use  shared `Crystal.shared` singleton to manage themes.
+///
+/// In order to use Crystal, you must make `Crystal` conform to `CrystalDetermined` protocol, which is also the opportunity for
+/// you to determine the concrete theme type.
+///
+/// Take a look at `CrystalDetermined` to learn about all the available methods and properties you can use.
 public class Crystal {
-    // Returns the singleton Crystal instance.
+    /// Returns the singleton Crystal instance.
     public static let shared = Crystal()
     
-    /// The concrete theme type can only be determined when `Crystal` conforming to `CrystalDetermined`, so we use this property
-    /// to store a theme of any possible types.
+    /// The concrete theme type can only be determined when `Crystal` conforming to `CrystalDetermined`, so we use this
+    /// property to store a theme of any possible types.
     var underlyingTheme: CrystalThemeType?
     
     /// A key-value map table. The key is a `CrystalCompatible` object and the value is a  `Crystalline` object.
@@ -22,10 +29,11 @@ public class Crystal {
     init() {}
     
     /// Return the theme of desired type.
-    /// - Returns: A theme.
     ///
     /// This method should not fail at any time. It's the developer's responsibility to guarantee that there is only one concrete theme
     /// type in Crystal.
+    ///
+    /// - Returns: A theme of desired type.
     func desiredTheme<T: CrystalThemeType>() -> T {
         guard let underlyingTheme = underlyingTheme else {
             // if `underlyingTheme` is `nil`, initialize it with the default theme.
@@ -45,10 +53,11 @@ public class Crystal {
 ///         public typealias Theme: AppTheme
 ///     }
 ///
-/// Although this protocol is designed for `Crystal` class, `Crystal` class does not conform to `CrystalDetermined` protocol by default.
-/// When using this library, it's your responsibility to conform `Crystal` class to `CrystalDetermined` protocol, in order to determine the
-/// concrete theme type.
+/// Although this protocol is designed for `Crystal` class, `Crystal` class does not conform to `CrystalDetermined` protocol
+/// by default. When using this library, it's your responsibility to make `Crystal` class conform to `CrystalDetermined` protocol,
+/// in order to determine the concrete theme type.
 public protocol CrystalDetermined: Crystal {
+    /// The theme type.
     associatedtype Theme: CrystalThemeType
     
     /// The current theme.
@@ -57,6 +66,7 @@ public protocol CrystalDetermined: Crystal {
     var theme: Theme { get set }
     
     /// Set the theme.
+    /// 
     /// - Parameters:
     ///   - theme: A theme to be adopted.
     ///   - animated: `true` to animate the theme change, `false` to make the change immediate.
@@ -91,21 +101,19 @@ extension CrystalDetermined where Self: Crystal {
 
 /// A type that has crystal extensions. You can use `cst` property to get extension methods.
 ///
-///     // Make `UIView` conform to `CrystalCompatible` to gain Crystal ability.
+///     // Make `UIView` conform to `CrystalCompatible`.
 ///     extension UIView: CrystalCompatible {
 ///         public typealias Theme: AppTheme
 ///     }
 ///
 /// When conforming to `CrystalCompatible`, you have to determine the concrete theme type in the conformance.
 public protocol CrystalCompatible: AnyObject {
-    associatedtype Target: AnyObject
+    /// The theme type that is determined when making protocol conformance.
     associatedtype Theme: CrystalThemeType
-    
-    /// Crystal extensions.
-    var cst: Crystalline<Target, Theme> { get }
 }
 
 extension CrystalCompatible {
+    /// Crystal namespace used to expose available methods and properties.
     public var cst: Crystalline<Self, Theme> {
         // Return the exist object.
         if let crystal = Crystal.shared.mapTable[self] as? Crystalline<Self, Theme> {
